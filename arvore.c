0a5f3913f58ec082b20811be580ea282c0cc17e3
douglasabraga <51207364+douglasabraga@users.noreply.github.com>
2019-09-23 14:29:23 -0300

    Add files via upload

--- arvore.c
+++ arvore.c
@@ -5,27 +5,29 @@ void inicializa(TNodo **R){
     (*R) = NULL;
 }
 //=================================================
-void insere(TNodo **R, char vlr[10]){
+void insere(TNodo **R, int id, char nome[50], float saldo){
     if((*R) == NULL){
-    	(*R) = geraNodo(vlr);
-    } else if (strcmp((*R)->valor, vlr) < 0){
+    	(*R) = geraNodo(id, nome, saldo);
+    } else if (strcmp((*R)->nome, nome) < 0){
     	 if((*R)->dir == NULL){
-    	 	(*R)->dir = geraNodo(vlr);
+    	 	(*R)->dir = geraNodo(id, nome, saldo);
     	 } else {
-    	 	insere(&(*R)->dir, vlr);
+    	 	insere(&(*R)->dir, id, nome, saldo);
     	 }
     } else {
     	if((*R)->esq == NULL){
-    		(*R)->esq = geraNodo(vlr);
+    		(*R)->esq = geraNodo(id, nome, saldo);
     	} else {
-    		insere(&(*R)->esq,vlr);
+    		insere(&(*R)->esq, id, nome, saldo);
     	}
     }
 }
 //=================================================
-TNodo *geraNodo(char v[10]){
+TNodo *geraNodo(int id, char nome[50], float saldo){
    TNodo *novo = (TNodo *)malloc(sizeof(TNodo));
-   strcpy(novo->valor,v);
+   novo->id = id;
+   strcpy(novo->nome,nome);
+   novo->saldo = saldo;
    novo->esq = NULL;
    novo->dir = NULL;
    return novo;
@@ -33,7 +35,7 @@ TNodo *geraNodo(char v[10]){
 //=================================================
 void preOrdem(TNodo *R){
 	if(R != NULL){
-		printf("\t%s",R->valor);
+		printf("\t%s %d",R->nome, R->id);
 		preOrdem(R->esq);
 		preOrdem(R->dir);
 	}//if
@@ -42,7 +44,7 @@ void preOrdem(TNodo *R){
 void emOrdem(TNodo *R){
 	if(R != NULL){
     	emOrdem(R->esq);
-		printf("\t%s",R->valor);
+		printf("\t%s",R->nome);
 		emOrdem(R->dir);
 	}//if
 }
@@ -51,7 +53,40 @@ void posOrdem(TNodo *R){
 	if(R != NULL){
     	posOrdem(R->esq);
 		posOrdem(R->dir);
-		printf("\t%s",R->valor);
+		printf("\t%s",R->nome);
 	}//if
 }
 //================================================
+
+// Esta função devolve o número de nós
+// da árvore binária cuja raiz é R.
+int count(TNodo *R){
+    if (R == NULL) return 0;
+    return count(R->esq) + count(R->dir) + 1;
+}
+
+// Devolve o altura de um nó R em uma árvore binária.
+
+int height(TNodo *R){
+    int u, v;
+    if (R == NULL) return -1;
+    u = height(R->esq);
+    v = height(R->dir);
+    if (u > v) return u + 1;
+    else return v + 1;
+}
+
+// Recebe uma árvore de busca h e um inteiro v.
+// Devolve um nó cuja chave é igual a v. 
+// Devolve NULL se tal nó não existe.
+//
+TNodo *searchR(TNodo *R, int v) {
+    int t;
+    if (R == NULL) return NULL;
+    t = R->id;
+    if (v == t) return R;
+    if (v < t) 
+       return searchR(R->esq, v);
+    else 
+       return searchR(R->dir, v);
+}
--- main.c
+++ main.c
@@ -3,21 +3,27 @@
 
 int main(){
 	inicializa(&raiz);
-	insere(&raiz,"PAULA");
-	insere(&raiz,"RUAN");
-	insere(&raiz,"VARLA");
-	insere(&raiz,"TITO");
-	insere(&raiz,"LIVIA");
+	insere(&raiz, 1, "PAULA", 123.12);
+	insere(&raiz, 2, "RUAN", 244.33);
+	insere(&raiz, 3, "VARLA", 312.11);
+	insere(&raiz, 4, "TITO", 42.12);
+	insere(&raiz, 5, "LIVIA", 512.12);
 	
 	printf("\n\n\nCaminhamento PRE_ORDEM:");
 	preOrdem(raiz);
 
-	printf("\n\n\nCaminhamento EM_ORDEM:");
-	emOrdem(raiz);
-
-	printf("\n\n\nCaminhamento POS_ORDEM:");
-	posOrdem(raiz);
+//	printf("\n\n\nCaminhamento EM_ORDEM:");
+//	emOrdem(raiz);
+//
+//	printf("\n\n\nCaminhamento POS_ORDEM:");
+//	posOrdem(raiz);
 	
+	int i = count(raiz);
+	int h = height(raiz);
+	TNodo *n = searchR(raiz, 1);
+	printf("\nRaiz: %s", n->nome);
+	printf("\ncount: %d", i);
+	printf("\naltura: %d", h);
 	printf("\n\n\n");
 	//system("PAUSE");
     getchar();
